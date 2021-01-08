7cc12d442fc7d04d7cdd25ecb2ca23cb2fe45d06
Hani Alfiyyah <hanii.alfiyyah@gmail.com>
2021-01-08 09:56:06 +0700

    test

--- client/assets/js/main.js
+++ client/assets/js/main.js
@@ -3,3 +3,17 @@ $(document).ready(function() {
     auth()
 })
 
+const afterLogin = () => {
+    $('#login-button').hide()
+    $('#register-button').hide()
+    $('#logout-button').show()
+    $('.recipe').show()
+    readRecipe()
+}
+
+const beforeLogin = () => {
+    $('#login-button').show()
+    $('#register-button').show()
+    $('#logout-button').hide()
+    $('.recipe').hide()
+}
